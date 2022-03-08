2cf41c8a8e594b663d7586fd3482b827f2b0dba6
Victor <vicbil33bdx@gmail.com>
2022-03-08 13:35:04 +0100

    test 64648

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-    echo "test";
+<head>
+    <title>Stage Sensor</title>
+    <meta charset="UTF-8">
+</head>
 
-?>
+<body>
+
+    <?php echo "test" ?>
+
+    <h2>cumlord</h2>
+
+    <h1>test gael</h1>
+
+</body>
+
+</html>
