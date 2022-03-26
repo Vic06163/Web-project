eee7c5c4b249bd42c4924e10b8cee3e5d4559819
Nicolas Kouyoumji <azonX33@gmail.com>
2022-03-26 17:32:46 +0100

    a

---
+++ Views/viewHomeMenu/viewHomeMenu.php
@@ -0,0 +1,14 @@
+<?php $titre = "login"; ?>
+<?php ob_start(); ?>
+
+<!-- Page body -->
+<link rel="stylesheet" href="css/logIn.css">
+
+<?php foreach ($users as $user): ?>
+    <h1><?= $user['Identifiant'] ?></h1>
+<?php endforeach; ?>
+
+<!-- Page body -->
+
+<?php $contenu = ob_get_clean(); ?>
+<?php require 'Views/template.php' ?>
