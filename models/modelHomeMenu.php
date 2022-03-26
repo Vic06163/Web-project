2cd6372e67bb4f0cb30e65229b3df7e13d2b9509
Nicolas Kouyoumji <azonX33@gmail.com>
2022-03-26 18:23:05 +0100

    l

--- models/modelHomeMenu.php
+++ models/modelHomeMenu.php
@@ -1,8 +1,13 @@
 <?php
 
 function getBdd() {
-    $bdd = new PDO('mysql:host=localhost:3307;dbname=stagesensor', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=localhost;dbname=stagesensor', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     return $bdd;
+}
+function getLastoffer() {
+    $bdd = getBdd();
+    $lastoffer = $bdd->query('SELECT Titre FROM offre ORDER BY date desc Limit 1');
+    return $lastoffer;
 }
 
 /* function getUsers() {
@@ -10,11 +15,4 @@ function getBdd() {
     $users = $bdd->query('SELECT Identifiant FROM utilisateur');
     return $users;
 } */
-
-function getLastoffer() {
-    $bdd = getBdd();
-    $lastoffer = $bdd->query('SELECT Titre FROM offre ORDER BY date desc Limit 1');
-    return $lastoffer;
-}
-
 ?>
