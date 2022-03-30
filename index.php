344095a5b78b64e768b699629147ae19f97376e9
Nicolas Kouyoumji <azonX33@gmail.com>
2022-03-30 14:38:47 +0200

    pwa

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<link rel="manifest" href="manifest.json">
+<link rel="apple-touch-icon" href="view/pictures/logophone/icon-96x96.png">
+  <meta name="apple-mobile-web-app-status-bar" content="white">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="theme-color" content="white">
+<script src="app.js"></script>
+
 <?php
 
 require_once 'views/viewLogin.php';
