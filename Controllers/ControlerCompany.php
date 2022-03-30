7a417ab08f2cf678c3a9030a53e13333caa4ef6f
tinkybeatz <gaedelouis@gmail.com>
2022-03-30 10:16:15 +0200

    ok

--- Controllers/ControlerCompany.php
+++ Controllers/ControlerCompany.php
@@ -7,16 +7,6 @@ function search(){
     require 'E:\2EME ANNEE\PROJET 4 WEB\PROJET\Livrable 3\Stage-Sensor\Views\viewCompany\viewSearchCompany.php';
 }
 
-function create(){
-    $resultQuery = createCompanies();
-    require 'E:\2EME ANNEE\PROJET 4 WEB\PROJET\Livrable 3\Stage-Sensor\Views\viewCompany\viewModifyCompany.php';
-}
-
-function modify(){
-    $resultQuery = modifyCompanies();
-    require 'E:\2EME ANNEE\PROJET 4 WEB\PROJET\Livrable 3\Stage-Sensor\Views\viewCompany\viewModifyCompany.php';
-}
-
 function ModifyDatabase(){
     $resultQuery = doModifyDatabase();
     require 'E:\2EME ANNEE\PROJET 4 WEB\PROJET\Livrable 3\Stage-Sensor\Views\viewCompany\viewModifyCompany.php';
